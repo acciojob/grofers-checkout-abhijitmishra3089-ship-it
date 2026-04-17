5a392f070d6812fe02ff99a2cf3b8af653b4f098
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-04-17 18:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+const prices=Document.quereySelectorAll('.prices');
+	let total=0;
+	prices.forEach((price)=>{
+  total+=Number(prices.textcontent);
+		});
+	const newrow=Documennt.createElement("tr");
+	const cell=Documennt.createElement('td');
+	cell.colspan=2;
+	cell.textcontent=total;
+	newrow.appendChild(newcell);
+    Element.querySelector('table').appendChild('newrow');
 };
 
 getSumBtn.addEventListener("click", getSum);
